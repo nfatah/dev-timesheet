9aa5144eb64edc2982ddee6ec7e3e3b8670f9ac1
nfatah <nfatah41@gmail.com>
2017-03-08 00:13:26 +0300

    fiix onclick by removing child component

--- src/components/timesheet/dev-timesheet.js
+++ src/components/timesheet/dev-timesheet.js
@@ -5,30 +5,41 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as timesheetActions from '../../actions/timesheet.actions.js';
 import Status from './timesheet-status';
-import ApproveButton from './approve-button';
 
 class DevTimesheet extends React.Component {
   constructor(props, context){
     super(props, context);
-
+    this.handleApprove = this.handleApprove.bind(this);
   }
-
+    
+  // }
+  handleApprove(e){
+    let {dispatch, timesheet} = this.props;
+    let selected_week = timesheet.selected_week;
+    let week_timesheet = timesheet.dev_timesheet.weeks;
+    week_timesheet = week_timesheet.filter(week => week.week_number == selected_week)[0];// de-array it
+    let week_id = week_timesheet.week_id;
+    let approved_by_id = this.refs.x.value;
+    console.log('clicked',approved_by_id);
+    dispatch(timesheetActions.approveDevTimesheet(week_id,approved_by_id));
+  }
   // componentDidMount(){
   //   $('#add_note').val('New Text');
   //   $('#add_note').trigger('autoresize');
   // }
   render(){
-    let selected_week = this.props.timesheet.selected_week;
-    let week_timesheet = this.props.timesheet.dev_timesheet.weeks;
-    week_timesheet = week_timesheet.filter(week => week.week_number == selected_week)[0];// de-array it
+    let {devs, dispatch, timesheet} = this.props;
+    let selected_week = timesheet.selected_week;
+    let month = timesheet.dev_timesheet.month;
+    let owner_id = timesheet.dev_timesheet.owner_id;
 
     //debugger;
     return(
       <div className="row">
         <div>
           <div className="container">
-              <h4 className="center">MONTH {this.props.timesheet.dev_timesheet.month} - WEEK {selected_week} TIMESHEET </h4>
-              <h5 className="center blue-text">Developer: User_{this.props.timesheet.dev_timesheet.owner_id}</h5>
+              <h4 className="center">MONTH {month} - WEEK {selected_week} TIMESHEET </h4>
+              <h5 className="center blue-text">Developer: User_{owner_id}</h5>
           </div>
 
           <divider></divider>
@@ -42,16 +53,16 @@ class DevTimesheet extends React.Component {
                         <thead>
                             <tr>
                               <th data-fiel="status">Status</th>
-                              <th data-fiel="approver">Approved by</th>
+                              <th data-fiel="approver">Approvers</th>
                               <th data-fiel="approved_date">Approve Date</th>
                             </tr>
                         </thead>
                         <tbody>
-                          <tr>
+                          {/*<tr>
                             <td><Status status={week_timesheet.status}/></td>
-                            <td>{week_timesheet.approvers.map(id => ` User_${id} |`)}</td>
+                            <td>{week_timesheet.approvers.map(id => `*User_${id}* `)}</td>
                             <td>{week_timesheet.approved_by_date}</td>
-                          </tr>
+                          </tr>*/}
                         </tbody>
                       </table>
                     </div>
@@ -69,7 +80,18 @@ class DevTimesheet extends React.Component {
                   <div className="card-action">
                     <div className="row">
                       <div className="col s6">
-                      <ApproveButton week_id={week_timesheet.week_id} approved={week_timesheet.status}/>
+                        <div className="input-field col s6">
+                          <select ref="x"className="browser-default" >
+                            <option defaultValue="approve_as" disabled>Approve as:</option>
+                            {devs.map((dev,index) => {
+                                return (
+                                  <option key={index}value={index+1}>{dev.username}</option>
+                                );
+                              })
+                            }
+                          </select>
+                        </div>
+                        <buton type="button" className="btn green" onClick={this.handleApprove}>APPROVE</buton>
                       </div>
                       <div className="col s6">
                         <buton type="button"className="btn red right">REJECT</buton>
@@ -88,7 +110,10 @@ class DevTimesheet extends React.Component {
 }
 // Props Validation
 DevTimesheet.propTypes = {
-  timesheet: PropTypes.object.isRequired
+  timesheet: PropTypes.object.isRequired,
+  devs: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+
 };
 
 // Connect Component wraps and exposes our components to redux
